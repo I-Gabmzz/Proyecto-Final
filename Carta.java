46866db6b51fe7254e9415d22f249e15aab5a94e
Dierik28 <diego.alfonso@uabc.edu.mx>
2025-05-16 22:46:41 -0700

    Se agregaron ciertos métodos a la clase Carta, estos pueden cambiar conforme se avance en el proyecto.

--- Carta.java
+++ Carta.java
@@ -1,2 +1,22 @@
 public class Carta {
+    private int valor;
+    private String color;
+    private String figura;
+
+    public Carta(int valor, String color, String figura) {
+        this.valor = valor;
+        this.color = color;
+        this.figura = figura;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getFigura() {
+        return figura;
+    }
+
 }
